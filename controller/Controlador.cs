cdb4cec89d673c82af96746f1fe7ad7bf2f72ad4
Fernando Alvarez <ferolsen@hotmail.com>
2021-09-23 13:18:38 -0600

    Bola robada.

---
+++ controller/Controlador.cs
@@ -0,0 +1,14 @@
+﻿using Caso2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caso2.controller
+{
+    internal class Controlador
+    {
+        List<Ball> balls;
+    }
+}
